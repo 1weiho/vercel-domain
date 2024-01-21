78fc78f0ab2e66730fb39325e9259cc319c798e1
David Ho <davidho0403@gmail.com>
2024-01-21 17:13:07 +0800

    chore: improve mobile rwd

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,9 @@ export const metadata: Metadata = {
 
 const Home = () => {
   return (
-    <main className="flex flex-col h-screen items-center">
+    <main className="flex flex-col items-center">
       <Nav />
-      <VercelLogo className="h-16 w-16 mt-56 lg:mt-72" />
+      <VercelLogo className="h-16 w-16 mt-40 lg:mt-72" />
       <h1 className="text-2xl lg:text-3xl font-bold mt-4">
         Vercel Domain Checker
       </h1>
