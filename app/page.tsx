9a5826b16d768bc18177ff0f31d8dde8d776943c
David Ho <davidho0403@gmail.com>
2024-01-21 15:21:01 +0800

    chore: add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
 import Nav from '@/components/nav';
 import SearchForm from '@/components/search-form';
 import VercelLogo from '@/components/vercel-logo';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  metadataBase: new URL('https://vercel-domain.vercel.app/'),
+  title: 'Vercel Domain Checker',
+  description: 'Check if a *.vercel.app domain is available on Vercel',
+  openGraph: {
+    title: 'Vercel Domain Checker',
+    description: 'Check if a *.vercel.app domain is available on Vercel',
+    url: 'https://vercel-domain.vercel.app/',
+    type: 'website',
+    images: '/og-image.png',
+  },
+};
 
 const Home = () => {
   return (
